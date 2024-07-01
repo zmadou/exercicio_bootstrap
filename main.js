41d9ef6176e99467388e861e03ca47c376ed78b5
zmadou <ferreira1337@hotmail.com>
2024-06-30 22:33:21 -0300

    formulário bootstrap

---
+++ main.js
@@ -0,0 +1,22 @@
+(() => {
+    'use strict'
+
+    const forms = document.querySelectorAll('.needs-validation')
+
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+        }
+        form.classList.add('was-validated')
+        }, false)
+    })
+})()
+
+$(document).ready(function(){
+
+    $('#telefone').mask('(00) 00000-0000', {
+        placeholder: '(__) _____-____'
+    })
+})
